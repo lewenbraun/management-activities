bf5d865a28fa7f5d9ced19b8f3c263f8eb821362
de-lolec <matvey20158@mail.ru>
2025-06-14 19:11:38 -0300

    init

---
+++ rector.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Caching\ValueObject\Storage\FileCacheStorage;
+use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
+use Rector\CodeQuality\Rector\FuncCall\CompactToVariablesRector;
+use Rector\CodeQuality\Rector\If_\SimplifyIfReturnBoolRector;
+use Rector\Config\RectorConfig;
+use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
+use Rector\Set\ValueObject\SetList;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddReturnTypeDeclarationRector;
+use Rector\ValueObject\PhpVersion;
+use RectorLaravel\Rector\Class_\AddExtendsAnnotationToModelFactoriesRector;
+use RectorLaravel\Rector\ClassMethod\AddGenericReturnTypeToRelationsRector;
+use RectorLaravel\Set\LaravelSetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__.'/app',
+        __DIR__.'/config',
+        __DIR__.'/database',
+        __DIR__.'/resources',
+        __DIR__.'/routes',
+        __DIR__.'/tests',
+    ]);
+
+    $rectorConfig->skip([
+        CompactToVariablesRector::class,
+        __DIR__.'/resources/views/vendor',
+    ]);
+
+    $rectorConfig->cacheDirectory(__DIR__.'/storage/rector');
+    $rectorConfig->cacheClass(FileCacheStorage::class);
+
+    $rectorConfig->sets([
+        SetList::CODE_QUALITY,
+        SetList::TYPE_DECLARATION,
+        LaravelSetList::LARAVEL_CODE_QUALITY,
+        LaravelSetList::LARAVEL_COLLECTION,
+    ]);
+
+    $rectorConfig->phpVersion(PhpVersion::PHP_84);
+
+    $rectorConfig->parallel();
+
+    $rectorConfig->importNames();
+    $rectorConfig->importShortClasses(false);
+
+    $rectorConfig->fileExtensions(['php']);
+
+    $rectorConfig->rules([
+        ClosureToArrowFunctionRector::class,
+        SimplifyIfReturnBoolRector::class,
+        AddReturnTypeDeclarationRector::class,
+        InlineConstructorDefaultToPropertyRector::class,
+        AddGenericReturnTypeToRelationsRector::class,
+        AddExtendsAnnotationToModelFactoriesRector::class,
+    ]);
+};
